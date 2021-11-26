91aec4573a45ebc18c2257e016dd9ce1d650ca22
JShivode <93088012+JShivode@users.noreply.github.com>
2021-11-26 16:24:44 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "my_mat.h"
 
+
 //A function that receives a matrix from the user:
 void getArr(int mat[N][N]){
 printf("\n Enter matrix cells");
@@ -33,6 +34,9 @@ for(int k=0; k<N; k++){
 for(int i=0; i<N; i++){
 for(int j=0; j<N; j++){
 arr[i][j]=min(arr[i][j], arr[i][k]+arr[k][j]);
+if(arr[i][j]==0){
+arr[i][j]=-1;
+}
 }
 }
 }
