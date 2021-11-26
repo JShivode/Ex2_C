4e385f1078e2e4845a21f5f40ff9f0468041134d
JShivode <93088012+JShivode@users.noreply.github.com>
2021-11-26 16:02:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,23 +7,33 @@
 
 
 int main(){
+printf("This is new");
 int i, j;
 int mat[N][N];
 char x='1';
 
 
 while (x !='0'){
-printf("What would you like to do\n A enter matrix\n B fined shortest paths matrix\n C fined shortest path from i to j\n  D print matrix \n 0 to exit\n");
+printf("What would you like to do\n A enter matrix\n B Enter i and j\n C find shortest path from i to j\n  D print matrix \n 0 to exit\n");
 scanf("%c", &x);
 if(x=='A'){
 getArr(mat);
 }
 
 if(x=='B'){
-floydWarshall(mat);
+printf("\n Enter i and j ");
+scanf("%d",&i);
+scanf("%d", &j);
+if(mat[i][j] !=0){
+printf("\nThere is a path from i to j\n");
+}
+else{
+printf("\nThere is no path\n");
+}
 }
 
 if(x=='C'){
+floydWarshall(mat);
 printf("\n Enter i and j ");
 scanf("%d",&i);
 scanf("%d", &j);
