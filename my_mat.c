0ee6f22970d302ee55b445f09805506920d6affd
JShivode <93088012+JShivode@users.noreply.github.com>
2021-11-27 22:21:12 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -4,7 +4,7 @@
 
 //A function that receives a matrix from the user:
 void getArr(int mat[N][N]){
-//printf("\n Enter matrix cells");
+printf("\n Enter matrix cells");
 for (int k=0; k<N; k++){
 for (int l=0; l<N; l++){
 scanf("%d", &mat[k][l]);
@@ -14,19 +14,14 @@ scanf("%d", &mat[k][l]);
 
 
 void printMat(int mat[N][N]){
-FILE *f;
 for (int i=0; i<N; i++){
 for(int j=0; j<N; j++){
-//printf("%d ", mat[i][j]);
-f=fopen("o1.txt","w" );
-fprintf(f,"%d",mat[i][j]);
+printf("%d ", mat[i][j]);
 if(j==N-1) {
-//printf("\n");
-fprintf(f,"\n");
+printf("\n");
 }
 }
 }
-fclose(f);
 }
 
 int min(int a, int b){
