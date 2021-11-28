031c3acb8d2bab52fee9f5065571a2fee6a1c894
JShivode <93088012+JShivode@users.noreply.github.com>
2021-11-28 11:46:19 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,11 +1,9 @@
 #ifndef N 
 #define N 10
 #endif
+
 //A function that receives a matrix from the user:
 void getArr();
-
-//A function that prints the matrix:
-void printMat();
 
 //returns the minnimum of 2 integers:
 int min(int a, int b);
