eb777495d694513ca629163da6b1fa2820f0027d
JShivode <93088012+JShivode@users.noreply.github.com>
2021-11-28 17:52:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,13 @@
 
 int main(){
 int i, j;
-int mat[N][N];
 char x='1';
 
 while (x !='D'){
 scanf("%c", &x);
 if(x=='A'){
 getArr(mat);
+floydWarshall();
 }
 
 if(x=='B'){
@@ -26,10 +26,15 @@ printf("False\n");
 }
 
 if(x=='C'){
-floydWarshall(mat);
 scanf("%d",&i);
 scanf("%d", &j);
+if(mat[i][j]==0){
+printf("-1\n");
+}
+else{
 printf("%d\n", mat[i][j]);
 }
 }
 }
+}
+
