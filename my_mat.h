ae0420457a5f28537b92c765315d724b53e82cb3
JShivode <93088012+JShivode@users.noreply.github.com>
2021-11-28 11:15:41 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -2,13 +2,13 @@
 #define N 10
 #endif
 //A function that receives a matrix from the user:
-void getArr(int mat[N][N]);
+void getArr();
 
 //A function that prints the matrix:
-void printMat(int mat[N][N]);
+void printMat();
 
 //returns the minnimum of 2 integers:
 int min(int a, int b);
 
 //the algorithm:
-void floydWarshall(int arr[N][N]);
+void floydWarshall();
